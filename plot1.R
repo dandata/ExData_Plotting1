d8557a69e72b924cc8d768437613eefcfdf4ee8d
Denis Baranov <dan.baranov@gmail.com>
2015-04-07 11:48:40 +0300

    added file download

--- plot1.R
+++ plot1.R
@@ -1,4 +1,8 @@
-data <- read.csv('household_power_consumption.txt', sep=';', stringsAsFactors=F)
+temp <- tempfile()
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', temp, method='curl')
+data <- read.csv(unz(temp, 'household_power_consumption.txt'), sep=';', stringsAsFactors=F, na.strings='?')
+unlink(temp)
+
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data.play <- subset(data, Date == '2007-02-01' | Date == '2007-02-02')
 
--- plot3.R
+++ plot3.R
@@ -1,4 +1,8 @@
-data <- read.csv('household_power_consumption.txt', sep=';', stringsAsFactors=F)
+temp <- tempfile()
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', temp, method='curl')
+data <- read.csv(unz(temp, 'household_power_consumption.txt'), sep=';', stringsAsFactors=F, na.strings='?')
+unlink(temp)
+
 data.play <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
 data.play$Datetime <- strptime(paste(data.play$Date, data.play$Time), format="%d/%m/%Y %H:%M:%S")
 
